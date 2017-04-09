62f8b785e7e239387712e5e6a97173e8e600a0dc
liz rush <liz@lizmrush.com>
2017-04-09 09:37:24 -0700

    Initial Commit

---
+++ meme-editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme-editor
+//
+//  Created by liz on 9/4/17.
+//  Copyright © 2017 liz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
